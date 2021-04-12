9031221a273219e03c04dfcfa9f9bc2035d096d6
sczajkowski <48910937+sczajkowski@users.noreply.github.com>
2021-04-12 14:35:13 +0200

    proba1

---
+++ Xamarin/Lab2/Phoneword/Phoneword/SharedResources.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms;
+
+namespace Phoneword
+{
+    public static class SharedResources
+    {
+        public static Color ButtonBkColor
+        {
+            get { return Color.Red; }
+        }
+
+        public static Color ButtonTextColor
+        {
+            get { return Color.White; }
+        }
+    }
+}
---
+++ Xamarin/lab3/xaml_api_db/xaml_api_db/xaml_api_db/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace xaml_api_db
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            btnPhoto.Clicked += btnPhoto_Clicked;
+        }
+
+        private async void btnPhoto_Clicked(object sender, EventArgs e)
+        {
+            var photo = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+            });
+            if (photo == null)
+                return;
+
+
+            imgPhoto.Source = ImageSource.FromStream(() => photo.GetStream());
+            byte[] bytes;
+            using (var memoryStream = new MemoryStream())
+            {
+                photo.GetStream().CopyTo(memoryStream);
+                bytes = memoryStream.ToArray();
+            }
+
+            string base64 = Convert.ToBase64String(bytes);
+            //person.PictureBase64 = base64;
+        }
+
+
+    }
+}
