c59343470ae7fb7bc205686834f5995203dce578
sczajkowski <48910937+sczajkowski@users.noreply.github.com>
2021-06-22 14:44:46 +0200

    rest api

---
+++ PROJEKT_IOT/Weather/Weather/WeatherApp/Constants.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace WeatherApp
+{
+    public static class Constants
+    {
+        public static string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+        public static string OpenWeatherMapAPIKey = "2b35650ce7cbe72fda4a00182e2ca32d";
+    }
+}
---
+++ PROJEKT_IOT/Weather/Weather/WeatherApp/RestService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace WeatherApp
+{
+    public class RestService
+    {
+        HttpClient _client;
+
+        public RestService()
+        {
+            _client = new HttpClient();
+        }
+        //http://api.openweathermap.org/data/2.5/forecast?q=Poznan&appid=2b35650ce7cbe72fda4a00182e2ca32d&units=metric
+        public async Task<WeatherData> GetWeatherData(string query)
+        {
+            WeatherData weatherData = null;
+            try
+            {
+                var response = await _client.GetAsync(query);
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    weatherData = JsonConvert.DeserializeObject<WeatherData>(content);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("\t\tERROR {0}", ex.Message);
+            }
+
+            return weatherData;
+        }
+    }
+}
